b228203b1cfecae8ba543f347682e0decc3be69b
Pablo0Matos <100294263+Pablo0Matos@users.noreply.github.com>
2022-03-02 13:15:52 -0500

    Add files via upload

---
+++ Tax Income Project.py
@@ -0,0 +1,18 @@
+#My First Project
+TR = 0.20 #Tax Rate
+SD = 10000.0 #Standar Deduction
+DD = 3000.0 #Dependent Deduction 
+
+GrossInc = float(input('Enter the gross income: '))
+NumD = int(input('Enter the number of dependents: '))
+
+D_Deduction = DD * NumD
+GI = GrossInc - SD #CurrentIncome
+IncomeTax = (GI - D_Deduction) * TR
+
+
+print('\n\nGross income:', GrossInc,
+      ('\nNumber of Depedents:'), NumD, 
+      ('\nTax income:'), IncomeTax)
+
+input('\nPress Enter to end')
